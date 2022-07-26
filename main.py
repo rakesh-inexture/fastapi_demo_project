4fa7609eb8fc17a13c118758f0a4f01c8a07316b
RAKESH KUMAR <rakesh.inexture@gmail.com>
2022-07-26 18:39:47 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,106 @@
+from enum import Enum
+from typing import Union
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"Hello Rakesh"}
+
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
+#
+
+# @app.put("/items/{item_id}")
+# async def create_item(item_id: int, item: Item):
+#     return {"item_id": item_id, **item.dict()}
+#
+
+# @app.put("/items/{item_id}")
+# async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+#     result = {"item_id": item_id, **item.dict()}
+#     if q:
+#         result.update({"q": q})
+#     return result
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+        *,
+        item_id: int,
+        item: Item,
+        user: User,
+        importance: int = Body(gt=0),
+        q: Union[str, None] = None
+):
+    results = {"item_id": item_id, "item": item, "user": User, "importance": importance}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+        item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
